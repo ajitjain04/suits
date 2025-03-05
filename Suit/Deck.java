7d259d48014fefe9068203221b2abe4412ea446a
Ajit Jain <jainajit787@gmail.com>
2025-03-05 15:42:42 +0530

    Method to createDeck

--- Suit/Deck.java
+++ Suit/Deck.java
@@ -8,4 +8,16 @@ class Deck{
 	public Deck(){
 		this.deck = new ArrayList<>();
 	}
+	
+	public void createDeck(){
+        	String[] suits = {"Hearts", "Diamonds", "Clubs", "Spades"};
+        	String[] ranks = {"2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"};
+		
+		for(String suit : suits){
+			for(String rank : ranks){
+				deck.add(new Card(suit,rank));
+
+			}
+		}
+	}
 }
