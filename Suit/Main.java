eff0a31e84a76938e94e4668836a9d92f992b4a5
Ajit Jain <jainajit787@gmail.com>
2025-03-05 15:49:26 +0530

    Method calling and object creation

--- Suit/Main.java
+++ Suit/Main.java
@@ -4,4 +4,25 @@
 import java.util.*;
 
 class Main {
+    public static void main(String args[]) {
+        Deck deck = new Deck();
+        System.out.println("*******Creating Deck********");
+        deck.createDeck();
+        System.out.println("*******Printing Deck********");
+        deck.displayDeck();
+        System.out.println("*******Shuffling Deck********");
+        deck.shuffleDeck();
+        deck.displayDeck();
+
+        System.out.println("******Distributing cards among 3 players****");
+        Player player1 = new Player("Player 1");
+        Player player2 = new Player("Player 2");
+        Player player3 = new Player("Player 3");
+
+        distributeCards(deck, player1, player2, player3);
+
+        player1.showHand();
+        player2.showHand();
+        player3.showHand();
+    }
 }
